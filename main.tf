e6f4636a59734003ab631abc1350efae2e07d88b
Tega McKinney <tega.mckinney@flexdrive.com>
2018-08-30 11:40:09 -0400

    hard code custom error responses

--- main.tf
+++ main.tf
@@ -119,6 +119,18 @@ resource "aws_cloudfront_distribution" "default" {
 
   aliases = ["${var.aliases}"]
 
+  custom_error_response {
+    error_code         = "404"
+    response_code      = "200"
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = "403"
+    response_code      = "200"
+    response_page_path = "/index.html"
+  }
+
   origin {
     domain_name = "${local.bucket_domain_name}"
     origin_id   = "${module.distribution_label.id}"
